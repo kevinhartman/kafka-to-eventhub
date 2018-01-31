6b663de737c5837432ce745847665258ed86d4b4
Kevin Hartman <kevin@hart.mn>
2018-01-31 17:57:59 -0500

    Make DefaultAdapter serializable.

--- src/main/scala/mn/hart/kafka2eventhub/DefaultAdapter.scala
+++ src/main/scala/mn/hart/kafka2eventhub/DefaultAdapter.scala
@@ -2,6 +2,6 @@ package mn.hart.kafka2eventhub
 
 import org.apache.kafka.clients.consumer.ConsumerRecord
 
-object DefaultAdapter extends ((ConsumerRecord[Array[Byte], Array[Byte]]) => Array[Byte]) {
+object DefaultAdapter extends ((ConsumerRecord[Array[Byte], Array[Byte]]) => Array[Byte]) with Serializable {
   override def apply(v1: ConsumerRecord[Array[Byte], Array[Byte]]): Array[Byte] = v1.value()
 }
