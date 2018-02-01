36ae30cb0212b81fa6419d62ce9d0d0671acebbd
Kevin Hartman <kevin@hart.mn>
2018-02-01 16:09:36 -0500

    Add EventHub connection arguments.

--- src/main/scala/mn/hart/kafka2eventhub/Arguments.scala
+++ src/main/scala/mn/hart/kafka2eventhub/Arguments.scala
@@ -4,10 +4,12 @@ import scala.concurrent.duration.Duration
 
 case class Arguments(
   batchDuration: Duration = null,
+  eventHubConnStr: String = null,
+  eventHubName: String = null,
   topics: Seq[String] = List(),
   brokerList: Option[String] = None,
-  zookeeper: Option[String] = null,
-  groupId: Option[String] = null,
+  zookeeper: Option[String] = None,
+  groupId: Option[String] = None,
   adapterFunctionClass: Option[String] = None,
   kafkaParamsClass: Option[String] = None,
   compression: Option[String] = None,
@@ -19,10 +21,19 @@ object Arguments {
     val parser = new scopt.OptionParser[Arguments]("kafka-to-eventhub") {
       head("kafka-to-eventhub", "1.x")
 
-      opt[Duration]("duration").required().valueName("<duration>").action((d, args) => args.copy(batchDuration = d))
+      opt[Duration]("duration").required().valueName("<duration>")
+        .action((d, args) => args.copy(batchDuration = d))
         .text("Spark Streaming batch duration")
 
-      opt[Seq[String]]("topics").required().valueName("<topic1>,[<topic2>...]").action((t, args) => args.copy(topics = t))
+      opt[String]("eventhub-connection-string").abbr("eh-conn").required().valueName("<connection string>")
+        .action((e, args) => args.copy(eventHubConnStr = e))
+        .text("EventHub connection string")
+
+      opt[String]("eventhub-name").abbr("eh-name").required().valueName("<name>")
+        .action((n, args) => args.copy(eventHubName = n))
+
+      opt[Seq[String]]("topics").required().valueName("<topic1>,[<topic2>...]")
+        .action((t, args) => args.copy(topics = t))
         .text("Kafka topics to forward into EventHub")
         .validate(t => if (t.nonEmpty) success else failure("Value <topic1> must be present"))
 
@@ -31,26 +42,32 @@ object Arguments {
         .text("Kafka broker list")
         .validate(b => if (b.nonEmpty) success else failure("Value <broker1> must be present"))
 
-      opt[String]("zookeeper").optional().valueName("<zookeeper>").action((z, args) => args.copy(zookeeper = Some(z)))
+      opt[String]("zookeeper").optional().valueName("<zookeeper>")
+        .action((z, args) => args.copy(zookeeper = Some(z)))
         .text("Zookeeper hostname")
 
-      opt[String]("group-id").optional().valueName("<groupid>").action((g, args) => args.copy(groupId = Some(g)))
+      opt[String]("group-id").optional().valueName("<groupid>")
+        .action((g, args) => args.copy(groupId = Some(g)))
         .text("Kafka group ID used for commit log")
 
-      opt[String]("adapter-object").optional().valueName("<namespace>.<objectname>").action((o, args) => args.copy(adapterFunctionClass = Some(o)))
+      opt[String]("adapter-object").optional().valueName("<namespace>.<objectname>")
+        .action((o, args) => args.copy(adapterFunctionClass = Some(o)))
         .text("fully qualified Scala root-level object name of function to use when converting deserialized data from Kafka to EventHub format")
 
-      opt[String]("kafka-params-object").optional().valueName("<namespace>.<objectname>").action((o, args) => args.copy(kafkaParamsClass = Some(o)))
+      opt[String]("kafka-params-object").optional().valueName("<namespace>.<objectname>")
+        .action((o, args) => args.copy(kafkaParamsClass = Some(o)))
         .text("fully qualified Scala root-level object name of function supplying a custom Kafak parameter map")
 
-      opt[String]("output-compression").optional().valueName("<format>").action((c, args) => args.copy(compression = Some(c)))
+      opt[String]("output-compression").optional().valueName("<format>")
+        .action((c, args) => args.copy(compression = Some(c)))
         .text("compress events sent to EventHub using <format> (currently supports 'gzip' only)")
         .validate(c => c.toLowerCase match {
           case "gzip" => success
           case _ => failure("Value <format> must be in the set: ('gzip')")
         })
 
-      opt[Unit]("force").optional().action((_, args) => args.copy(force = true))
+      opt[Unit]("force").optional()
+        .action((_, args) => args.copy(force = true))
         .text("skip validation of Kafka parameters")
 
       note(
