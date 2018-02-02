c5583ab7ef0b0cfe6541bc2ea61c3b19f8c4e5b2
Kevin Hartman <kevin@hart.mn>
2018-02-02 17:04:10 -0500

    Add workaround for bug github.com/Azure/azure-event-hubs-java/issues/235.

--- src/main/scala/mn/hart/kafka2eventhub/UploadToEventHub.scala
+++ src/main/scala/mn/hart/kafka2eventhub/UploadToEventHub.scala
@@ -1,14 +1,29 @@
 package mn.hart.kafka2eventhub
 
-import com.microsoft.azure.eventhubs.EventData
+import com.microsoft.azure.eventhubs.{EventData, EventDataBatch}
 import org.apache.spark.rdd.RDD
 
 import scala.collection.JavaConverters._
 import scala.language.implicitConversions
+import scala.util.{Failure, Success, Try}
 
 object UploadToEventHub {
   private implicit def iteratorToIterable[A](iterator: java.util.Iterator[A]): java.lang.Iterable[A] = {
     iterator.asScala.toIterable.asJava
+  }
+
+  /**
+    * Workaround for https://github.com/Azure/azure-event-hubs-java/issues/235
+    *
+    * TODO: delete this extension once fix is released.
+    * @param value original [[EventDataBatch]] object on which to bind extension method.
+    */
+  implicit class EventDataBatchWorkaround(val value: EventDataBatch) extends AnyVal {
+    def tryAddWorkaround(event: EventData): Boolean = Try(value.tryAdd(event)) match {
+      case Success(result) => result
+      case Failure(_: java.nio.BufferOverflowException) => false
+      case Failure(unexpectedEx) => throw unexpectedEx
+    }
   }
 
   def apply(arguments: Arguments, rdd: RDD[EventData]): Unit = {
@@ -22,7 +37,7 @@ object UploadToEventHub {
         while (partition.hasNext) {
           val event = partition.next
 
-          if (!batch.tryAdd(event)) {
+          if (!batch.tryAddWorkaround(event)) {
             if (batch.getSize > 0) {
               // Send filled batch to EventHub
               client.sendSync(batch.iterator)
@@ -32,7 +47,7 @@ object UploadToEventHub {
             }
 
             // Retry adding event into empty batch
-            if (!batch.tryAdd(event)) {
+            if (!batch.tryAddWorkaround(event)) {
               // Single event > 256KB. For now, send alone to trigger correct message too big error.
               client.sendSync(event)
             }
